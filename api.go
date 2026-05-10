536ecde7549574be8913cf2c95cae17a327ba567
RayhanMarcello <rayhanmarcello222@gmail.com>
2026-05-10 11:52:27 +0800

    create handler for createUsers

--- api.go
+++ api.go
@@ -10,13 +10,13 @@ type api struct {
 }
 
 type User struct {
-	ID   int
-	Name string
+	Firstname string
+	Lastname  string
 }
 
 var users = []User{}
 
-func (a *api) getUserHandler(w http.ResponseWriter, r *http.Request) {
+func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// encode users slice to json
@@ -25,5 +25,23 @@ func (a *api) getUserHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 		return
 	}
+	w.WriteHeader(200)
+}
+
+func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request) {
+	var payload User
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), 404)
+		return
+	}
+
+	u := User{
+		Firstname: payload.Firstname,
+		Lastname:  payload.Lastname,
+	}
+
+	users = append(users, u)
+
 	w.WriteHeader(200)
 }
